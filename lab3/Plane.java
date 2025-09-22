8bc12df331e86d8326ae82e534211cfe18e91436
sukkho <AIYA001@e.ntu.edu.sg>
2025-09-22 16:41:00 +0800

    week 7 lab

--- lab3/Plane.java
+++ lab3/Plane.java
@@ -14,23 +14,93 @@ public class Plane {
         }
     }
 
+    private PlaneSeat[] sortSeats() {
+        //create a copy for modification
+        PlaneSeat[] sortedSeats = new PlaneSeat[12];
+        for (int i = 0; i <12; i++) {
+            sortedSeats[i] = seats[i];
+        }
+
+        //bubble sort by customerID
+        for (int i = 0; i < sortedSeats.length -1; i++) {
+            for (int j = 0; j < sortedSeats.length -i - 1; j++) {
+                if (sortedSeats[j].getCustomerID() > sortedSeats[j+1].getCustomerID()) {
+                    PlaneSeat temp = sortedSeats[j];
+                    sortedSeats[j] = sortedSeats[j+1];
+                    sortedSeats[j+1] = temp;
+                }
+            }
+        }
+        return sortedSeats;
+    }
+
     public void showNumEmptySeats() {
         System.out.println("Number of empty seats: " + numEmptySeat);
     }
 
     public void showEmptySeats() {
+        System.out.println("Empty seats: ");
+        boolean foundEmpty = false; //assume seat is occupied
 
+        for (int i = 0; i < seats.length; i++) {
+            PlaneSeat currentSeat = seats[i];
+            if (!currentSeat.isOccupied()) {
+                System.out.println("Seat " +currentSeat.getSeatID());
+                foundEmpty = true;
+            }
+        }
+
+        if (!foundEmpty) { //ie foundEmpty = false -> FULL
+            System.out.println("No empty seats available.");
+        }
     }
 
     public void showAssignedSeats(boolean bySeatId) {
+        System.out.println("Assigned Seats: ");
 
+        if(bySeatId) { //show by seat ID
+            for (PlaneSeat seat : seats) {
+                if (seat.isOccupied()) {
+                    System.out.println("Seat " + seat.getSeatID() + " assigned to Customer " + seat.getCustomerID());
+                }
+            }
+
+
+        } else { // show by customer ID
+            PlaneSeat[] sortedSeats = sortSeats();
+            for (PlaneSeat seat : sortedSeats) {
+                if (seat.isOccupied()) {
+                    System.out.println("Seat " + seat.getSeatID() + " assigned to Customer " + seat.getCustomerID());
+                }
+            }
+        }
     }
 
     public void assignSeat(int seatId, int cust_id) {
+        if (seatId < 1 || seatId > 12) {
+            System.out.println("Invalid seat number!");
+            return;
+        }
+
+        PlaneSeat seat = seats[seatId - 1];
+        if (!seat.isOccupied()) {
+            seat.assign(cust_id);
+            numEmptySeat--;
+            System.out.println("Seat "+seatId+" assigned to customer "+cust_id);
+        } else {
+            System.out.println("Seat " + seatId+ " is already occupied!");
+        }
 
     }
 
     public void unAssignSeat(int seat_id) {
-
+        PlaneSeat seat = seats[seat_id - 1];
+        if (seat.isOccupied()) {
+            seat.unAssign();
+            numEmptySeat++;
+            System.out.println("Seat " + seat_id + " unassigned!");
+        } else {
+            System.out.println("Seat " + seat_id + " is already empty!");
+        }
     }
 }
--- lab3/PlaneApp.java
+++ lab3/PlaneApp.java
@@ -1,7 +1,59 @@
 package lab3;
+import java.util.Scanner;
 
 public class PlaneApp {
     public static void main(String[] args) {
-        
-    }
+        Scanner sc = new Scanner(System.in);
+        Plane plane = new Plane();
+        int choice;
+
+        System.out.println("(1) Show number of empty seats");
+        System.out.println("(2) Show the list of empty seats");
+        System.out.println("(3) Show the list of seat assignments by seat ID");
+        System.out.println("(4) Show the list of seats assignments by customer ID");
+        System.out.println("(5) Assign a customer to a seat");
+        System.out.println("(6) Remove a seat assignment");
+        System.out.println("(7) Exit");
+
+        do {
+            System.out.print("Enter the number of your choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    plane.showNumEmptySeats();
+                    break;
+                case 2:
+                    System.out.println("The following seats are empty:");
+                    plane.showEmptySeats();
+                    break;
+                case 3:
+                    System.out.println("The seat assignment are as follow:");
+                    plane.showAssignedSeats(true);
+                    break;
+                case 4:
+                    System.out.println("The seat assignments are as follow:");
+                    plane.showAssignedSeats(false);
+                    break;
+                case 5:
+                    System.out.println("Assigning Seat ..");
+                    System.out.print("Please enter SeatID: ");
+                    int seatId = sc.nextInt();
+                    System.out.print("Please enter Customer ID: ");
+                    int custId = sc.nextInt();
+                    plane.assignSeat(seatId, custId);
+                    break;
+                case 6:
+                    System.out.print("Enter SeatID to unassign customer from: ");
+                    int seat_id = sc.nextInt();
+                    plane.unAssignSeat(seat_id);
+                    break;
+                case 7:
+                    System.out.println("Exiting...");
+                    sc.close();
+                default:
+                    System.out.println("Invalid choice. Please try again.\n");
+                    break;
+            }
+        } while (choice != 7);
+    } 
 }
